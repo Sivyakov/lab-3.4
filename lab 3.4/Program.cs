2ce4de7676e6af445a1e8ab3ef6b19aac145dc5e
UserXM230121 <UserXM230121@DESKTOP-591DAC7>
2021-11-23 17:13:10 +0600

    Добавьте файлы проекта.

---
+++ lab 3.4/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace lab_3._4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine("|     x       |      y       |");
+                for (double x = 1; x <= 3; x += 0.2)
+                {
+                    double y = x * x * x - 7 * x - 7;
+                    Console.WriteLine("|{0,13:F1}|{1,14:F2}|", x, y);
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
